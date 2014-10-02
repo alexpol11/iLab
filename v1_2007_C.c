f5c8e7fa5451062d85bf4f9f5a8aa20f939575d0
alexpol11 <alexpol11@mail.ru>
2014-10-02 17:12:57 +0400

    Version 1; Year: 2007; Variant 1; № C

---
+++ v1_2007_C.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int func_Unnamed(int number){
+    if (((number + 2) % 4) == 0){
+        return 0;
+    }
+    else{
+        if ((number == 4)||(number == 1)){
+            return 1;
+        }
+        else{
+            if (((number - 1) % 4) == 0){
+                return func_Unnamed((number - 1) / 4);
+            }
+            else{
+                if ((number % 4) == 0){
+                    return func_Unnamed((number) / 4);
+                }
+                else{
+                    return func_Unnamed((number + 1) / 4);
+                }
+            }
+        }
+    }
+}
+
+void printAnswer(int boolean){
+    if (boolean){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+}
+
+int scanNumber(void){
+    int number = 0;
+    scanf("%d", &number);
+    return number;
+}
+
+int main(){
+    int num = 0, boolean;
+    printf("Waiting for number...\n");
+
+    num = scanNumber();
+    boolean = func_Unnamed(num);
+    printAnswer(boolean);
+
+    system("pause");
+    return 0;
+}
