f52987018f380380c35e28ca9c569b7d89986102
alexpol11 <alexpol11@mail.ru>
2014-11-07 03:33:29 +0300

    исправил размер стэка, добавил функции swap u dump

--- my_stack.c
+++ my_stack.c
@@ -3,27 +3,31 @@
 #include <stdio.h>
 #include <stddef.h>
 
-//
-// функций swap и dump нет, тк не понял что они делают
-//
-//
+void EXIT(my_stack_double* stack, int errCode){
+    my_stack_double_delete(stack);
+    exit(errCode);
+}
+
+void my_stack_double_dump(my_stack_double* stack){
+    int i = 0;
+    while(&stack->data[i] != stack->stack_pointer){
+        printf("data[%d] = %0.2f\n", i, stack->data[i]);
+        i++;
+    }
+}
 
 void killUncorrectStack(my_stack_double* stack, char* progPath){
     if (stack == NULL){
         printf("%s ERROR: Stack doesn't exist!\n", progPath);
-        exit(1);
+        EXIT(stack, 1);
     }
     if (stack->stack_pointer == NULL){
         printf("%s ERROR: NULL stack pointer!\n", progPath);
-        exit(2);
-    }
-    if (stack->size == 0){
-        printf("%s ERROR: NULL stack size!\n", progPath);
-        exit(3);
+        EXIT(stack, 2);
     }
     if (stack->data == NULL){
         printf("%s ERROR: Data points on NULL adress!\n", progPath);
-        exit(4);
+        EXIT(stack, 4);
     }
 }
 
@@ -40,37 +44,51 @@ size_t my_stack_double_size(my_stack_double* stack){
 
 my_stack_double* my_stack_double_new(size_t size){
     if (size == 0) {
-        printf("CONSTRUCTOR ERROR: You can't create NULL size stack!\n");
-        exit(5);
+        return my_stack_double_new(1);
     }
-    my_stack_double* stack = (my_stack_double*)calloc(1, sizeof(stack));
+    my_stack_double* stack = (my_stack_double*)calloc(1, sizeof(*stack));
     if (stack == NULL){
-        printf("CONSTRUCTOR ERROR: Stack create failed!\n");
+        perror("CONSTRUCTOR ERROR: Stack create failed!\n");
         exit(6);
     }
-
+    
     stack->size = size;
     stack->data = (double*)calloc(stack->size, sizeof(*stack->data));
     stack->stack_pointer = stack->data;
 
     if (stack->data == NULL) {
-        printf("CONSTRUCTOR ERROR: Stack create failed!\n");
-        exit(7);
+        perror("CONSTRUCTOR ERROR: Stack create failed!\n");
+        EXIT(stack, 7);
     }
     return stack;
 }
 
 int my_stack_double_push(my_stack_double* stack, double data){
     killUncorrectStack(stack, "PUSH");
+    
     if (stack->stack_pointer == &stack->data[stack->size - 1] + 1) {
-        printf("PUSH ERROR: Stack overflow!\n");
-        exit(1);
+        double* tmpData = (double*)malloc((stack->size+1)*sizeof(*tmpData));
+        
+        if (stack->data == NULL){
+            perror("PUSH ERROR: Stack data allocation failed!\n");
+            exit(8);
+        }
+        
+        int i = 0;
+        while (i != stack->size) {
+            tmpData[i] = stack->data[i];
+            i++;
+        }
+        free(stack->data);
+        stack->data = tmpData;
+        stack->stack_pointer = &stack->data[stack->size];
+        stack->size++;
     }
     *stack->stack_pointer = data;
     stack->stack_pointer++;
     if (my_stack_double_empty(stack)){
         printf("PUSH ERROR: Push result: empty stack!\n");
-        exit(8);
+        EXIT(stack, 9);
     }
     return 0;
 }
@@ -81,7 +99,7 @@ double my_stack_double_pop(my_stack_double* stack){
 
     if (my_stack_double_empty(stack)) {
         printf("POP ERROR: Empty stack!\n");
-        exit(9);
+        EXIT(stack, 10);
     }
 
     stack->stack_pointer--;
@@ -94,6 +112,7 @@ double my_stack_double_pop(my_stack_double* stack){
 int my_stack_double_delete(my_stack_double* stack){
     if (stack == NULL){
         printf("DESTRUCTOR ERROR: Stack doesn't exist!\n");
+        return -1;
     }
     stack->stack_pointer = NULL;
     free(stack->data);
@@ -107,10 +126,18 @@ double my_stack_double_top(my_stack_double* stack){
 
     if (my_stack_double_empty(stack)) {
         printf("TOP ERROR: Empty stack!\n");
-        exit(10);
+        EXIT(stack, 11);
     }
 
     element = *(stack->stack_pointer - 1);
 
     return element;
+}
+
+void swap(my_stack_double* stack){
+    double num1 = 0, num2 = 0;
+    num1 = my_stack_double_pop(stack);
+    num2 = my_stack_double_pop(stack);
+    my_stack_double_push(stack, num2);
+    my_stack_double_push(stack, num1);
 }
